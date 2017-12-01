2f6908ffd637c3b2b627ce6b1c139383e15d62a2
Daniel <dpt53@cornell.edu>
2017-12-01 12:30:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import nltk as nl
+import pandas as pd
+import numpy as np
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+import string
+import re
+import pickle
+from urllib.request import urlopen
+import ssl
