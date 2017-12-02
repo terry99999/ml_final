ec654be6afde17da340a968bb40ebcc22af93ecf
Evan Kaplan <ekaplan6@gatech.edu>
2017-12-02 16:58:08 -0500

    Update to include Dan's data

--- evan_code/combine_joe_evan_dan.py
+++ evan_code/combine_joe_evan_dan.py
@@ -3,6 +3,7 @@ import csv
 
 evan_sub = np.loadtxt(open("evan.csv", "rb"), delimiter=",").astype(int)
 joe_sub = np.loadtxt(open("joe.csv", "rb"), delimiter=",").astype(int)
+dan_sub = np.loadtxt(open("dan_int.csv", "rb"), delimiter=",").astype(int)
 
 output = []
 for i in range(len(evan_sub)):
@@ -11,10 +12,12 @@ for i in range(len(evan_sub)):
         arr[evan_sub[i][j]] = arr[evan_sub[i][j]] + (20 - j)*1.05
     for j in range(len(joe_sub[0])):
         arr[joe_sub[i][j]] = arr[joe_sub[i][j]] + (20 - j)
-    output.append(np.argsort(arr)[::-1][:20])    
+    for j in range(len(dan_sub[0])):
+        arr[dan_sub[i][j]] = arr[dan_sub[i][j]] + (20 - j)
+    output.append(np.argsort(arr)[::-1][:20])
 output = np.array(output)
 
-f = open('submission_final_evan_joe.csv', 'wt')
+f = open('submission_final_evan_joe_dan.csv', 'wt')
 writer = csv.writer(f)
 writer.writerow(('Descritpion_ID','Top_20_Image_IDs'))
 for i in range(len(output)):
